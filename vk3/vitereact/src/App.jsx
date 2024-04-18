a65069d1bff026f986640a784195b9e055bd46d0
SpookySkoops <rautiainen.oskarir@gmail.com>
2024-04-18 20:26:01 +0300

    fix

--- vk3/vitereact/src/App.jsx
+++ vk3/vitereact/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from 'rautiainenoskari.github.io/web-tehtavat/vk3/vitereact/src/assets/react.svg'
-import viteLogo from 'rautiainenoskari.github.io/web-tehtavat/vk3/vitereact/vite.svg'
-import 'rautiainenoskari.github.io/web-tehtavat/vk3/vitereact/src/App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
