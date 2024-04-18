87789617cc5e4cfcd6b2acc8958814d868a36c52
SpookySkoops <rautiainen.oskarir@gmail.com>
2024-04-18 20:14:41 +0300

    pathit

--- vk3/vitereact/src/main.jsx
+++ vk3/vitereact/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from 'rautiainenoskari.github.io/web-tehtavat/vk3/vitereact/src/App.jsx'
+import 'rautiainenoskari.github.io/web-tehtavat/vk3/vitereact/src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
